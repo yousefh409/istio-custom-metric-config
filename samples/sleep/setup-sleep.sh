f49d1db10e877529f87aab4d16afa2bb3b592a99
Yousef Helal <yousefh409@gmail.com>
2022-06-24 01:15:09 +0300

    reorganized files such that setup scripts for each sample app

--- samples/sleep/setup-sleep.sh
+++ samples/sleep/setup-sleep.sh
@@ -1,3 +1,5 @@
+# cd back into PWD
+cd ..
 # Starting minikube to store k8's cluster
     # Note: Need to update if using some other method
 # minikube start --cpus 4 --memory 8192
@@ -10,8 +12,13 @@ cd ..
 # setting up istio config
 istioctl install -f istio-config.yaml -y # -d istio-1.14.1/manifests -y
 kubectl label namespace default istio-injection=enabled
-# wait for all deployments to be rolled out
+
+# Setting up sample k8's cluster
+    # Note: Not needed if cluster already setup, or not using sample app
+kubectl apply -f samples/sleep/sleep.yaml
+# check that all services have been rolled out
 python3 wait-deployment-rollout.py
+
 # upload the extra stats configuration to each deployment
 python3 apply-deployment-stats.py   
 # check that all services have been rolled out
